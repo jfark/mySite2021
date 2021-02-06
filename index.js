bb01ae58ee8fc9eb75a2cb2229fbed26bd8a7a29
jfark <jessamyn.farkas@gmail.com>
2021-02-06 01:28:42 -0500

    Copying over from Capstone

---
+++ index.js
@@ -0,0 +1,77 @@
+let films = [];
+
+const perspectiveOrigin = {
+  x: parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue(
+      "--scenePerspectiveOriginX"
+    )
+  ),
+  y: parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue(
+      "--scenePerspectiveOriginY"
+    )
+  ),
+  maxGap: 10
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+      window.addEventListener("scroll", moveCamera);
+      window.addEventListener("mousemove", moveCameraAngle);
+      setSceneHeight();
+});
+
+
+
+function moveCamera() {
+  document.documentElement.style.setProperty("--cameraZ", window.pageYOffset);
+}
+
+function setSceneHeight() {
+  const numberOfItems = 8;
+  const itemZ = parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue("--itemZ")
+  );
+  const scenePerspective = parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue(
+      "--scenePerspective"
+    )
+  );
+  const cameraSpeed = parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue("--cameraSpeed")
+  );
+
+  const height =
+    window.innerHeight +
+    scenePerspective * cameraSpeed +
+    itemZ * cameraSpeed * numberOfItems;
+
+  document.documentElement.style.setProperty("--viewportHeight", height);
+}
+
+function moveCameraAngle(event) {
+  const xGap =
+    (((event.clientX - window.innerWidth / 2) * 100) /
+      (window.innerWidth / 2)) *
+    -1;
+  const yGap =
+    (((event.clientY - window.innerHeight / 2) * 100) /
+      (window.innerHeight / 2)) *
+    -1;
+  const newPerspectiveOriginX =
+    perspectiveOrigin.x + (xGap * perspectiveOrigin.maxGap) / 100;
+  const newPerspectiveOriginY =
+    perspectiveOrigin.y + (yGap * perspectiveOrigin.maxGap) / 100;
+
+  document.documentElement.style.setProperty(
+    "--scenePerspectiveOriginX",
+    newPerspectiveOriginX
+  );
+  document.documentElement.style.setProperty(
+    "--scenePerspectiveOriginY",
+    newPerspectiveOriginY
+  );
+}
+
+
+
+
